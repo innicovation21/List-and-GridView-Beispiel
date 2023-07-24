cf849a333e96f1732d025b4add269edd266fbf23
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-24 22:36:30 +0200

    Create listen.dart

---
+++ variables/listen.dart
@@ -0,0 +1,13 @@
+
+
+// Liste mit den Pfaden zu unseren Bildern der Charaktere
+
+List<String> avengersList = [
+  "images/groot.jpeg",
+  "images/ironman.jpeg",
+  "images/wong.jpeg",
+  "images/thor.jpeg",
+  "images/wanda.jpg",
+  "images/widow.jpg",
+  "images/rescue.jpg"
+];
