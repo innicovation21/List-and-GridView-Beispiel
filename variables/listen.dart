473b2efe843649a0308164c803f04ed28a6c8dbf
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-25 23:41:13 +0200

    Update listen.dart

--- variables/listen.dart
+++ variables/listen.dart
@@ -1,3 +1,5 @@
+import 'package:images_listview_gridview_sizer/datamodels/movies.dart';
+
 import '../datamodels/characters.dart';
 
 // hier eine Liste mit unseren Charakteren
@@ -22,4 +24,30 @@ List<Character> heroes = [
   Character("Wanda", "images/wanda.jpg"),
   Character("Black Widow", "images/widow.jpg"),
   Character("Rescue", "images/rescue.jpg")
+];
+
+// Liste mit Characters, damit wir neben dem Pfad
+// auch einen Namen angeben und abrufen können
+List<Character> villains = [
+  Character("Doc Oc", "images/villains/doctoroctopus.jpg"),
+  Character("Galactus", "images/villains/galactus.jpg"),
+  Character("Hela", "images/villains/hela.webp"),
+  Character("Loki", "images/villains/loki.jpeg"),
+  Character("Red Skull", "images/villains/redskull.png"),
+  Character("Sabretooth", "images/villains/sabretooth.jpg"),
+  Character("Thanos", "images/villains/thanos.jpg"),
+  Character("Ultron", "images/villains/ultron.jpeg")
+];
+
+// Liste mit Movies, damit wir neben dem Pfad
+// auch das Jahr angeben und abrufen können
+List<Movie> movies = [
+  Movie("images/movies/a_aou.jpg", "2015"),
+  Movie("images/movies/antman.jpg", "2015"),
+  Movie("images/movies/capt_marvel.jpg", "2019"),
+  Movie("images/movies/blackwidow.jpg", "2021"),
+  Movie("images/movies/shang_chi.webp", "2021"),
+  Movie("images/movies/sm_ffh.png", "2021"),
+  Movie("images/movies/thor_lat.jpeg", "2022"),
+  Movie("images/movies/gotg3.webp", "2023"),
 ];
