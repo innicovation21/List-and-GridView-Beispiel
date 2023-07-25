e4f7de08b9fc6c4e600a07e224d22bb414b3da23
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-25 23:41:35 +0200

    Add files via upload

---
+++ datamodels/movies.dart
@@ -0,0 +1,7 @@
+// Klasse für Filme, damit wir neben dem Pfad das Erscheinungsjahr angeben / abrufen können
+class Movie {
+  final String image;
+  final String year;
+
+  Movie(this.image, this.year);
+}
