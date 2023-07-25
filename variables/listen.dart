bd274f87b0f04715c547f62212e60da528f03bb9
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-25 09:52:01 +0200

    Update listen.dart

--- variables/listen.dart
+++ variables/listen.dart
@@ -1,7 +1,7 @@
+import '../datamodels/characters.dart';
 
-
-// Liste mit den Pfaden zu unseren Bildern der Charaktere
-
+// hier eine Liste mit unseren Charakteren
+//  nutzen wir später nicht mehr
 List<String> avengersList = [
   "images/groot.jpeg",
   "images/ironman.jpeg",
@@ -10,4 +10,16 @@ List<String> avengersList = [
   "images/wanda.jpg",
   "images/widow.jpg",
   "images/rescue.jpg"
+];
+
+// Liste mit Characters, damit wir neben dem Pfad
+// auch einen Namen angeben und abrufen können
+List<Character> heroes = [
+  Character("Groot", "images/groot.jpeg"),
+  Character("Iron Man", "images/ironman.jpeg"),
+  Character("Wong", "images/wong.jpeg"),
+  Character("Thor", "images/thor.jpeg"),
+  Character("Wanda", "images/wanda.jpg"),
+  Character("Black Widow", "images/widow.jpg"),
+  Character("Rescue", "images/rescue.jpg")
 ];
