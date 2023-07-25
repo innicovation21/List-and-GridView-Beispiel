3568ecfd450082c07c83523f48783027eb491de9
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-25 23:44:13 +0200

    Delete movies.dart

--- movies.dart
+++
@@ -1,7 +0,0 @@
-// Klasse für Filme, damit wir neben dem Pfad das Erscheinungsjahr angeben / abrufen können
-class Movie {
-  final String image;
-  final String year;
-
-  Movie(this.image, this.year);
-}
