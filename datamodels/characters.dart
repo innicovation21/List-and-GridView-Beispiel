7146693a46cea2f2f4d5b8ea812f8b7e407092d3
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-07-25 09:51:39 +0200

    Create characters.dart

---
+++ datamodels/characters.dart
@@ -0,0 +1,7 @@
+// Klasse für Charaktere, damit wir neben dem Pfad auch einen Namen zuweisen können
+class Character {
+  final String name;
+  final String image;
+
+  Character(this.name, this.image);
+}
